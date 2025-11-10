d848d6f3a0f597c68586cfc2b5affdb2e9911653
lsw-1234 <dlq4444@gmail.com>
2025-11-10 23:28:30 +0900

    mypage

--- src/main/java/com/project/trip/mypage/controller/MyPageController.java
+++ src/main/java/com/project/trip/mypage/controller/MyPageController.java
@@ -1,15 +1,45 @@
 package com.project.trip.mypage.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.project.trip.mypage.mapper.MyPageMappper;
+import com.project.trip.mypage.model.UserDTO;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class MyPageController {
 
+    @Autowired
+    private final MyPageMappper mapper;
+	
 	@GetMapping("/member/mypage")
 	public String mypage() {
 		
 		return "mypage.member.mypage";
 	}
+	
+
+	
+	@GetMapping("/member/userinfo")
+	    public String userInfo(HttpSession session, Model model) {
+	        String seq = session.getAttribute("seq").toString();
+
+	        UserDTO dto = new UserDTO();
+	        dto = mapper.userInfo(seq);
+
+	        System.out.println(dto);
+
+	        model.addAttribute("dto", dto);
+
+			return "mypage.member.userinfo";
+	    }
+	
 	
 }
---
+++ src/main/java/com/project/trip/mypage/mapper/MyPageMappper.java
@@ -0,0 +1,9 @@
+package com.project.trip.mypage.mapper;
+
+import com.project.trip.mypage.model.UserDTO;
+
+public interface MyPageMappper {
+
+	UserDTO userInfo(String seq);
+
+}
