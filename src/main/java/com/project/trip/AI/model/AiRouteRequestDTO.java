91e27f86c7f4e08be21e9364c503ae63c4859449
bunohoq <boyoung5187@naver.com>
2025-11-11 14:43:53 +0900

    Airoute 만드는중

---
+++ src/main/java/com/project/trip/AI/model/AiRouteRequestDTO.java
@@ -0,0 +1,22 @@
+package com.project.trip.AI.model;
+
+import lombok.Data;
+
+@Data
+public class AiRouteRequestDTO {
+	
+	private String city;
+	private String duration;
+	private String travelStyle;
+	private String activityTime;
+	private String buduget;
+	private String preferredArea;
+	private String transpotation;
+	private String acctivityType;
+	private String compation;
+	
+	private String startDate;
+	private String endDate;
+	
+
+}
--- src/main/java/com/project/trip/AI/service/AiRouteService.java
+++ src/main/java/com/project/trip/AI/service/AiRouteService.java
@@ -2,4 +2,5 @@ package com.project.trip.AI.service;
 
 public interface AiRouteService {
 
+
 }
